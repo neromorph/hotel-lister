aa7a0a5075039c2d7b9dfcb5629679b1fa86a96c
neromorph <nurmufid.255@gmail.com>
2023-04-01 08:36:31 +0700

    fix password

--- entities/user.go
+++ entities/user.go
@@ -5,7 +5,7 @@ import "time"
 type User struct {
 	ID         int       `json:"id"`
 	Username   string    `json:"username" binding:"required"`
-	Password   string    `json:"-" binding:"required"`
+	Password   string    `json:"password" binding:"required"`
 	Email      string    `json:"email"`
 	Created_at time.Time `json:"created_at"`
 	Updated_at time.Time `json:"updated_at"`
